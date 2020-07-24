35f20abf56cdbae666c8928c2eb5e8bbfbbe5eb3
cgalvezv <camilogalvezv@gmail.com>
2020-07-24 00:01:56 -0400

    feature(snackbar.constant): add constant file with snackbar msgs

---
+++ src/app/shared/snackbar.constant.ts
@@ -0,0 +1,4 @@
+export const SNACKBAR_HAS_EMPTY_FIELDS_MSG = 'Debe rellenar todos los campos para poder suscribirse a nuestro newslatter';
+export const SNACKBAR_GENERATING_SUSCRIPTION_MSG = 'Creando Suscripción ...';
+export const SNACKBAR_SUCCESFULLY_SUSCRIPTION_MSG = '{email} suscrito al newslatter de manera exitosa!';
+export const SNACKBAR_WRONG_SUSCRIPTION_MSG = 'Ha habido un error al querer inscribir la casilla {email}';
